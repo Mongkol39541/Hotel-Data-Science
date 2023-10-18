28a06bd2da7d18f4ebc59f6a11b2fb0e872ab91d
PimdaowKhayankan <145738097+PimdaowKhayankan@users.noreply.github.com>
2023-10-18 15:30:43 +0700

    Add files via upload

---
+++ customer/connect.php
@@ -0,0 +1,23 @@
+<!-- เชื่อมต่อฐานข้อมูล MySQLi Procedural -->
+<!-- In process : ยังไม่ได้ใช้ server สำหรับส่งงานจริง -->
+
+<?php
+
+if($open_connect != 1){
+    die(header("location: index.php")); //ป้องกันการเข้าถึงไฟล์
+}
+
+// กำหนด parameters สำหรับเชื่อมต่อฐานข้อมูล
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "9hotel_reservation";
+
+// Create & Check connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
