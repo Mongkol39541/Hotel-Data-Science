675dbb1408b591e02869e285e1b38789a9cb6637
cappuccinoako <chacuttbudee.k@gmail.com>
2023-10-22 09:23:15 +0700

    minor

--- customer/home/manage_res.js
+++ customer/home/manage_res.js
@@ -1,8 +1,15 @@
 // changing form when clicked edit information
+// no for loop gang
 function edit_info() {
+    $('#use-member-address').prop('disabled',false);
     $('#fname').prop('disabled',false);
     $('#lname').prop('disabled',false);
     $('#email').prop('disabled',false);
     $('#address').prop('disabled',false);
-    $('#use-member-address').prop('disabled',false);
+    $('#address').prop('disabled',false);
+    $('#date_editor').prop('disabled',false);
+    $('#delete').remove();
+    $('#edit').remove();
+    const test = createElement('<p>test</p>');
+    $('$lowest_button').appendChild(test);
 }
